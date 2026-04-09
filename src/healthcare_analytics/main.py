5eda35ef54dfe2cf25c5bc7f271ee5d606126e91
gnugal314 <techgeek2013@gmail.com>
2026-04-08 20:28:54 -0500

    Update main.py

--- src/healthcare_analytics/main.py
+++ src/healthcare_analytics/main.py
@@ -2,6 +2,10 @@ from healthcare_analytics.claims.orchestrator import run_claims_pipeline
 from healthcare_analytics.pharmacy_340b.orchestrator import run_pharmacy_pipeline
 
 
-if __name__ == "__main__":
+def main() -> None:
     run_claims_pipeline()
     run_pharmacy_pipeline()
+
+
+if __name__ == "__main__":
+    main()
