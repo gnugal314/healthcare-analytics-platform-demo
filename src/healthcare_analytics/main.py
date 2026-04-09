0a1fcf8e32e5efde85586091ae7da23b4192fa7c
gnugal314 <techgeek2013@gmail.com>
2026-04-08 19:51:23 -0500

    Import and run claims and pharmacy pipelines

--- src/healthcare_analytics/main.py
+++ src/healthcare_analytics/main.py
@@ -1,1 +1,7 @@
+from healthcare_analytics.claims.orchestrator import run_claims_pipeline
+from healthcare_analytics.pharmacy_340b.orchestrator import run_pharmacy_pipeline
 
+
+if __name__ == "__main__":
+    run_claims_pipeline()
+    run_pharmacy_pipeline()
